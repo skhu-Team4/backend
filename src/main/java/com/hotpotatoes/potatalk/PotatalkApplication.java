81b0419f0473fc77e927fe48090c149ac5fe539f
Park, seun <seunpark04@gmail.com>
2024-11-10 23:25:20 +0900

    Initial setting

---
+++ src/main/java/com/hotpotatoes/potatalk/PotatalkApplication.java
@@ -0,0 +1,13 @@
+package com.hotpotatoes.potatalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotpotatoes/potatalk/PotatalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotpotatoes.potatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
