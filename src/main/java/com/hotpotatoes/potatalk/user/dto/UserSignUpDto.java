fe824a38062092ddc9f493decba26a4a3fa12405
seok1213 <leeggoong222@gmail.com>
2025-01-30 13:18:56 +0900

    fix: 비밀번호 제약 통합

--- src/main/java/com/hotpotatoes/potatalk/user/dto/UserSignUpDto.java
+++ src/main/java/com/hotpotatoes/potatalk/user/dto/UserSignUpDto.java
@@ -13,12 +13,8 @@ public class UserSignUpDto {
 
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상, 20자 이하이어야 합니다.")
     @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$",
-            message = "비밀번호는 최소 하나의 알파벳과 숫자를 포함해야 하며, 특수문자는 사용할 수 없습니다."
-    )
-    @Pattern(
-            regexp = "^[^ㄱ-ㅎ가-힣]*$",
-            message = "비밀번호에 한글을 포함할 수 없습니다."
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?!.*[ㄱ-ㅎ가-힣])[A-Za-z\\d]+$",
+            message = "비밀번호는 최소 하나의 알파벳과 숫자를 포함해야 하며, 특수문자와 한글은 사용할 수 없습니다."
     )
     private String password;
     private String phoneNumber;
