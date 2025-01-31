0190b0704fc9c4d545f3c7db338571051f5ff2fc
Park, seun <seunpark04@gmail.com>
2025-01-31 15:12:20 +0900

    Chore: swagger info 기능 제외

--- src/main/java/com/hotpotatoes/potatalk/common/config/SwaggerConfig.java
+++ src/main/java/com/hotpotatoes/potatalk/common/config/SwaggerConfig.java
@@ -22,14 +22,7 @@ public class SwaggerConfig {
                 );
                 return new OpenAPI()
                         .components(new Components())
-                        .info(apiInfo())
                         .addSecurityItem(securityRequirement)
                         .components(components);
-        }
-        private Info apiInfo() {
-                return new Info()
-                        .title("API Test") // API의 제목
-                        .description("Let's practice Swagger UI") // API에 대한 설명
-                        .version("1.0.0"); // API의 버전
         }
 }
