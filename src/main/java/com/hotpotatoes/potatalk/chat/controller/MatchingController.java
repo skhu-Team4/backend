ab51755b3aa0315abe2ff913d67ca11c725f65f7
Park, seun <seunpark04@gmail.com>
2025-01-31 15:21:22 +0900

    chore: controller 어노테이션 변경 (Controller > RestController)

--- src/main/java/com/hotpotatoes/potatalk/chat/controller/MatchingController.java
+++ src/main/java/com/hotpotatoes/potatalk/chat/controller/MatchingController.java
@@ -9,8 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MatchingController {
 
